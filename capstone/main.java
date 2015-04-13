4e1617fcc94439f168aded300b27bcc8973c3f9b
Adam321Cad <aratoad@gmail.com>
2015-04-13 11:27:04 -0500

    4yw
    t4wa

---
+++ capstone/main.java
@@ -0,0 +1,7 @@
+
+public class main{
+    public static void main(String[] args){
+        String url = "http://finance.yahoo.com/q?uhb=uh3_finance_vert&fr=&type=2button&s=GOOG%2C";
+        websiteCatch w = new WebsiteCatch(url);
+    }
+}
---
+++ capstone/websiteCatch.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+import org.jsoup.examples.*;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class websiteCatch{
+    public websiteCatch(String urlName){
+        Jsoup j = new Jsoup();
+    }
+}
