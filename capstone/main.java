f64e6d535210beb3b5e7e308a1d83dbb2bf67347
Adam321Cad <aratoad@gmail.com>
2015-04-14 11:19:36 -0500

    yt
    yqa

--- capstone/main.java
+++ capstone/main.java
@@ -2,6 +2,6 @@
 public class main{
     public static void main(String[] args){
         String url = "http://finance.yahoo.com/q?uhb=uh3_finance_vert&fr=&type=2button&s=GOOG%2C";
-        websiteCatch w = new WebsiteCatch(url);
+        websiteCatch w = new websiteCatch(url);
     }
 }
--- capstone/websiteCatch.java
+++ capstone/websiteCatch.java
@@ -1,4 +1,7 @@
 import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import org.jsoup.examples.*;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
@@ -7,6 +10,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class websiteCatch{
     public websiteCatch(String urlName){
-        Jsoup j = new Jsoup();
+        String html = "";
+        String bdex = "<span id=\"yfs_l84_goog\">";
+        String adex = "</span></span>";
+        try{
+            URL url = new URL(urlName);
+            URLConnection con = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String l;
+            while ((l=in.readLine())!=null) {
+                //System.out.println(html);
+                html = html+l+"\n";
+            }
+            //System.out.println(html);
+        }catch(Exception e){
+            System.out.println("welp");
+        }
+        int start = html.indexOf(bdex);
+        int end = html.indexOf(adex);
+        String test = html.substring(start,end);
+        System.out.println(test);
     }
 }
