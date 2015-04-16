c522c86c8685ce9d59b2ac0b447c72f7de5d9d5e
Adam321Cad <aratoad@gmail.com>
2015-04-16 12:29:32 -0500

    r4
    a4ywe

--- capstone/webdriver.java
+++ capstone/webdriver.java
@@ -1,9 +1,12 @@
+//import org.openqa.selenium.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 /**
  * Write a description of class webdriver here.
@@ -22,7 +25,7 @@ public class webdriver
     
     public void main(){
          WebDriver driver = new FirefoxDriver();
-         driver.get("url");
+         driver.get(url);
     }
     
     
